cddaffef4a65a6f43a936ab7813130f4073b8f73
Jaime Boden <jaimeboden91@gmail.com>
2021-08-30 14:26:49 +0100

    Added test dependencies

---
+++ app/src/test/java/com/jaime/marvelviewer/RepositoryTests.kt
@@ -0,0 +1,26 @@
+package com.jaime.marvelviewer
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.After
+
+import org.junit.Before
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class RepositoryTests {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun start(){
+
+    }
+
+    @After
+    fun testDown() {
+
+    }
+}
