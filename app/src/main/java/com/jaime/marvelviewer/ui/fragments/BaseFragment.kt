8de3c564ba704b3d1f8eaa93e9880465774a8e9d
Jaime Boden <jaimeboden91@gmail.com>
2021-09-01 18:32:54 +0100

    Added base fragment comment

--- app/src/main/java/com/jaime/marvelviewer/ui/fragments/BaseFragment.kt
+++ app/src/main/java/com/jaime/marvelviewer/ui/fragments/BaseFragment.kt
@@ -7,6 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
+/**
+ * Base Fragment class to abstract away ViewBinding boilerplate code
+ * All fragments will inherit from this, only needing to override [initOnViewCreated] when desired functionality happens
+ */
 abstract class BaseFragment<VB: ViewBinding>: Fragment() {
     private var _binding: VB? = null
     val binding get() = _binding!!
@@ -19,8 +23,7 @@ abstract class BaseFragment<VB: ViewBinding>: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = bindingInflater.invoke(inflater, container, false)
-        val view = binding.root
-        return view
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
